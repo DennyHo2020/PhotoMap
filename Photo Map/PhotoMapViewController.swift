9bf1adc357fde8998141bb845144db2057656c49
DennyHo2020 <dennyho@email.arizona.edu>
2018-10-20 23:07:12 -0700

    done

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var pickedImage: UIImage!
+    var annotations = [MKAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +29,82 @@ class PhotoMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didPressCameraButton(_ sender: Any) {
+        let photoLibraryView = UIImagePickerController()
+        photoLibraryView.delegate = self
+        photoLibraryView.allowsEditing = true
+        photoLibraryView.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            photoLibraryView.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            photoLibraryView.sourceType = .photoLibrary
+        }
+        self.present(photoLibraryView, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        //let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        pickedImage = originalImage
 
+        //print(editedImage)
+        dismiss(animated: true) {
+            self.performSegue(withIdentifier: "tagSegue", sender: nil)
+        }
+    }
+    
     
     // MARK: - Navigation
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = UIImage(named: "camera")
+        
+        return annotationView
+    }
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let locationsView = segue.destination as? LocationsViewController
+        locationsView?.delegate = self
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+    func addPin(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let annotation = PhotoAnnotation()
+        annotation.photo = self.pickedImage
+        let locationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        annotation.coordinate = locationCoordinate
+        self.mapView.addAnnotation(annotation)
+        self.annotations.append(annotation)
+        self.mapView.showAnnotations(self.annotations, animated: true)
+    }
     
-
+    //Detect an annotation press
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let annotation = view.annotation {
+            if let title = annotation.title! {
+                print("Tapped \(title) pin")
+            }
+        }
+    }
 }
+
+extension PhotoMapViewController: LocationsViewControllerDelegate {
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+        self.addPin(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+    }
+}
+
+
